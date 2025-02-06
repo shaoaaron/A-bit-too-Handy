9a3422a0c77c0e2edbe9c8a4a2973386a98e6cf0
Aaron Shao <72823620+shaoaaron@users.noreply.github.com>
2025-02-05 21:18:17 -0500

    loop through files

--- cleaning.py
+++ cleaning.py
@@ -1,10 +1,16 @@
 import os
-import cv2
-import numpy as np
 
+train_path = "C:\Users\aaron\OneDrive\Desktop\Projects\a-bit-too-handy\A-bit-too-Handy\archive (1)\train\train"
+val_path = "C:\Users\aaron\OneDrive\Desktop\Projects\a-bit-too-handy\A-bit-too-Handy\archive (1)\val\val"
+img_size = 224
 
+print("Checking path:", train_path)
+print("Folders inside train:", os.listdir(train_path))
 
+for subfolder in os.listdir(train_path):
+    subfolder_path = os.path.join(train_path, subfolder)
 
-
-
-
+    if os.path.isdir(subfolder_path):  # Ensure it's a folder
+        for image_file in os.listdir(subfolder_path):  # Iterate through images
+            image_path = os.path.join(subfolder_path, image_file)
+            print(f"Found image: {image_path}")  # Debugging output
